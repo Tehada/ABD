e8fd73b5d1ec2f9cc0f787664216321a6c190602
Alexander Sharipov <compmonk@yandex.ru>
2017-09-23 20:56:39 +0300

    Add draft functions

--- HW-1/main.cpp
+++ HW-1/main.cpp
@@ -1,20 +1,32 @@
 #include <iostream>
 #include <string>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <random>
 
+#define FILE_SIZE 10 //10 gb
+#define SEQ_SEGM_SIZE 50 //2 gb
+#define RND_SEGM_SIZE 5 //200 mb
 
-int main(int argc, char** argv) {
-    if (argc < 2) {
-        std::cerr << "Incorrect number of arguments: " << argc << ", expected >=2" << std::endl;
-        return 1;
-    }
+const char* filename = "BigFile";
 
-    //create big file on sdd
-    int fd = open("BigFile", O_RDWR | O_CREAT);
-    write(fd, )
+int main() {
+    int fd = open(filename, O_RDWR | O_CREAT | O_TRUNC); //O_SYNC, O_DIRECT
+    char* buf = (char*) malloc(FILE_SIZE); //incorrect!
+    write(fd, buf, FILE_SIZE);
+    fdatasync(fd); //or fsync()
+    close(fd);
 
-    std::string mode = argv[1];
+    //std::mt19937 generator;
+
+    int fd = open(filename, O_RDWR | O_CREAT | O_APPEND);
+    std::string mode = "seq-read";
     if (mode == "seq-read") {
-        std::cerr << "58 MB/s\n"; 
+        char* buf = malloc(SEQ_SEGM_SIZE);
+        read(fd, buf, SEQ_SEGM_SIZE); //OFFSET?
+        //empty cache buffers?
+        //time
     } else if (mode == "seq-write") {
         std::cerr << "68 MB/s\n"; 
     } else if (mode == "rnd-read") {
@@ -34,3 +46,16 @@ int main(int argc, char** argv) {
 
     return 0;
 }
+
+/*
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        std::cerr << "Incorrect number of arguments: " << argc << ", expected >=2" << std::endl;
+        return 1;
+    }
+
+    //create big file on sdd
+    write(fd, )
+
+    }
+*/

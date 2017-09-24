210cb5d8f76a074d96566ba0fd748197c048e73e
Alexander Sharipov <compmonk@yandex.ru>
2017-09-24 17:57:14 +0300

    Ready for testing seqRead

--- HW-1/main.cpp
+++ HW-1/main.cpp
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include <random>
 #include <chrono>
+#include <iomanip>
 
 const off_t FILE_SIZE = std::pow(10, 8);//10gb == pow(10, 10);
 const size_t SEQ_SEGM_SIZE = std::pow(10, 7);//2gb == 2 * pow(10, 9);
@@ -20,8 +21,12 @@ void err() {
 
 void dropCaches() {
     sync();
-    std::ostream ofs("/proc/sys/vm/drop_caches");
-    ofs << "3" << std::endl;
+    //std::ofstream ofs("/proc/sys/vm/drop_caches");
+    //ofs << "3" << std::endl;
+    int fd = open("/proc/sys/vm/drop_caches", O_WRONLY);
+    if (fd == -1) err();
+    if (write(fd, "3", sizeof(char)) == -1) err();
+    close(fd);
 }
 
 void createFile(const char* filename, off_t size) {
@@ -32,33 +37,42 @@ void createFile(const char* filename, off_t size) {
     if (ftruncate(fd, size) == -1) err();
     //write sth to file???
     fdatasync(fd); //or fsync()
-    close(fd);//necessary?
-    //return fd;
-    std::cout << "file created!\n";
+    close(fd);
 }
 
 void seqRead() {
+    using std::chrono::steady_clock;
+    using std::chrono::duration_cast;
+    using std::chrono::nanoseconds;
+
     int fd = open(filename, O_RDWR);
     if (fd == -1) err();
     char* buf = (char*) malloc(SEQ_SEGM_SIZE);
     if (buf == NULL) err();
     if (lseek(fd, 0, SEEK_SET) == -1) err();
 
-    int accum = 0;
+    int64_t accum = 0;
     for (int i = 0; i < N_ITER; ++i) {
-        auto start = std::chrono::steady_clock::now();
+        auto start = steady_clock::now();
         if (read(fd, buf, SEQ_SEGM_SIZE) != SEQ_SEGM_SIZE) err(); 
-        auto diff = std::chrono::steady_clock::now() - start;
-        accum += diff.count();
+        auto diff = steady_clock::now() - start;
+        std::cout << "iter " << i << ": " << diff.count() << " "
+            << duration_cast<nanoseconds>(diff).count() << "\n";
+        accum += duration_cast<nanoseconds>(diff).count();
     }
 
     free(buf);
-    dropCaches();
     close(fd);
+    //dropCaches();
 
-    std::cout << "-mean time: " << accum / N_ITER << std::endl;
+    double data = SEQ_SEGM_SIZE / std::pow(10, 6);
+    double time = accum / N_ITER / std::pow(10, 9);
+    std:: cout << data << " " << time << std::endl;
+    std::cout << std::fixed << std::setprecision(1)
+              << data / time << " MB/s\n";
 }
 
+/*
 void seqWrite() {
     int fd = open(filename, O_RDWR);
     if (fd == -1) err();
@@ -66,22 +80,23 @@ void seqWrite() {
     if (buf == NULL) err();
     if (lseek(fd, 0, SEEK_SET) == -1) err();
 
-    int accum = 0;
+    std::chrono::milliseconds accum(0);
     for (int i = 0; i < N_ITER; ++i) {
         auto start = std::chrono::steady_clock::now();
         if (write(fd, buf, SEQ_SEGM_SIZE) != SEQ_SEGM_SIZE) err(); 
-        auto diff = std::chrono::steady_clock::now() - start;
-        accum += diff.count();
+        accum = std::chrono::steady_clock::now() - start;
+        //accum += diff;
     }
 
     free(buf);
-    dropCaches();
     close(fd);
+    //dropCaches();
 
-    std::cout << "time: " << diff.count() << std::endl;
-    std::cerr << "68 MB/s\n";
+    double data = SEQ_SEGM_SIZE / std::pow(10, 6);
+    double time = accum.count() / N_ITER / std::pow(10, 6);
+    std::cout << data / time << " MB/s\n";
 } 
-
+*/
 void rndRead() {
     
     //std::mt19937 generator;
@@ -99,7 +114,7 @@ int main() {
     if (mode == "seq-read") {
         seqRead();
     } else if (mode == "seq-write") {
-        seqWrite();
+        //seqWrite();
     } else if (mode == "rnd-read") {
         rndRead();
     } else if (mode == "rnd-write") {
